a381cd54a9597d5c7c4b07cec60c032a7367ca24
Trigg Summs <stsumms@gmail.com>
2020-08-24 08:33:11 -0500

    PlaceNames finished

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,5 +1,28 @@
+//import React, { useState } from "react";
+//import { Route, Link } from "react-router-dom";
+
+
 function main() {
-  // Put your code here
+
+names = [ "Nashville", "Hong Kong", "The back yard", "Earth",
+"London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+for (let name  of names) {
+  console.log(`${name}.`);
+}
+console.log("blank space", name);
+
+
+
+
+theNames = names; 
+theNames.find((the)=>the.theNames == "The");
+
+console.log("answerToStartingThe", the );
+
+for (let name2  of theNames) {
+  console.log(`${name2}`);
+}
+
 
 }
 
